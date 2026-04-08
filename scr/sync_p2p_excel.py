214463313911d7d9d3cebc8bc2f1abec88908f4c
Alana Olivieri Rincón <alana.olivieri@gmail.com>
2026-04-08 16:11:38 +0200

    updated sync file

--- scr/sync_p2p_excel.py
+++ scr/sync_p2p_excel.py
@@ -2,16 +2,25 @@ import pandas as pd
 import subprocess
 import os
 from datetime import datetime
+from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / ".env")
 
 ORIGEN = os.getenv("ORIGEN")
 REPO = os.getenv("REPO")
+
+print("CWD:", os.getcwd())
+print("BASE_DIR:", BASE_DIR)
+print("ORIGEN:", ORIGEN)
+print("REPO:", REPO)
+
 DESTINO = os.path.join(REPO, "p2p_latest.xlsx")
 
 df = pd.read_excel(ORIGEN, sheet_name="Data 2026")
-df.fillna('', inplace=True)
-df.drop('Count', axis = 1, inplace=True)
+# df.fillna('', inplace=True)
+df.drop('Count', axis=1, inplace=True, errors='ignore')
 df.to_excel(DESTINO, index=False)
 
 subprocess.run(["git", "add", "p2p_latest.xlsx"], cwd=REPO, check=True)
